39abc9d2f765d4de702f61c098c5e0ba03e9d2a9
ThariduJayaratne <34066349+ThariduJayaratne@users.noreply.github.com>
2019-12-09 09:07:44 +0000

    Update stencil.c

--- stencil.c
+++ stencil.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
+#include "mpi.h"
 
-#define NROWS 256
-#define NCOLS 1024
+// #define NROWS
+// #define NCOLS
 #define MASTER 0
+
+int calc_ncols_from_rank(int rank, int size);
+
 // Define output file name
 #define OUTPUT_FILE "stencil.pgm"
 
@@ -16,47 +20,48 @@ void output_image(const char* file_name, const int nx, const int ny,
                   const int width, const int height, float* image);
 double wtime(void);
 
-int calc_ncols_from_rank(int rank, int size);
-
 int main(int argc, char* argv[])
 {
-   int rank;              /* the rank of this process */
-   int up;              /* the rank of the process to the left */
-   int down;             /* the rank of the process to the right */
-   int size;              /* number of processes in the(local_nrows + local_ncols)*local_nrows) communicator */
-   int tag = 0;           /* scope for adding extra information to a message */
-   MPI_Status status;     /* struct used by MPI_Recv */
-   int local_nrows;       /* number of rows apportioned to this rank */
-   int local_ncols;       /* number of columns apportioned to this rank */
-   int remote_ncols;      /* number of columns apportioned to a remote rank */
-   double *sendbuf= (double*)malloc(sizeof(double) * local_nrows);       /* buffer to hold values to send */
-   double *recvbuf= (double*)malloc(sizeof(double) * local_ncols);       /* buffer to hold received values */
+  int ii,jj;             /* row and column indices for the grid */
+  int rankings;                /* index for looping over ranks */
+  int rank;              /* the rank of this process */
+  int up;              /* the rank of the process to the left */
+  int down;             /* the rank of the process to the right */
+  int size;              /* number of processes in the communicator */
+  int tag = 0;           /* scope for adding extra information to a message */
+  MPI_Status status;     /* struct used by MPI_Recv */
+  int local_nrows;       /* number of rows apportioned to this rank */
+  int local_ncols;       /* number of columns apportioned to this rank */
   // Check usage
-  MPI_Init( &argc, &argv );
-  MPI_Comm_size( MPI_COMM_WORLD, &size );
-  MPI_Comm_rank( MPI_COMM_WORLD, &rank );
-
   if (argc != 4) {
     fprintf(stderr, "Usage: %s nx ny niters\n", argv[0]);
     exit(EXIT_FAILURE);
   }
 
+  MPI_Init( &argc, &argv );
+  MPI_Comm_size( MPI_COMM_WORLD, &size );
+  MPI_Comm_rank( MPI_COMM_WORLD, &rank );
+
   up = (rank == MASTER) ? (rank + size - 1) : (rank - 1);
   down = (rank + 1) % size;
+  if(rank== MASTER){
+    up = MPI_PROC_NULL;
+  }
+  if(rank== size -1){
+    down = MPI_PROC_NULL;
+  }
+
   // Initiliase problem dimensions from command line arguments
   int nx = atoi(argv[1]);
   int ny = atoi(argv[2]);
-  int niters = 4;
+  int niters = atoi(argv[3]);
 
-  //for each section
-  int local_nrows = nx/4;
-  int local_ncols = ny + 2;
+  local_nrows = calc_nrows_from_rank(rank, size)+2;
+  local_ncols = ny + 2;
   if (local_ncols < 1) {
     fprintf(stderr,"Error: too many processes:- local_ncols < 1\n");
     MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
   }
-  // remote_ncols = calc_ncols_from_rank(size-1, size);
-
   // we pad the outer edge of the image to avoid out of range address issues in
   // stencil
   int width = nx + 2;
@@ -65,54 +70,72 @@ int main(int argc, char* argv[])
   // Allocate the image
   float* image = malloc(sizeof(double) * width * height);
   float* tmp_image = malloc(sizeof(double) * width * height);
-  float* section = malloc(sizeof(double) * local_nrows * local_ncols);
-  float* tmp_section = malloc(sizeof(double) * local_nrows * local_ncols);
+  float *section = malloc(sizeof(double) * local_nrows * local_ncols);
+  float *tmp_section = malloc(sizeof(double) * local_nrows * local_ncols);
+  // double* sendbuf  = malloc(sizeof(double) * local_ncols);
+  // double* recvbuf  = malloc(sizeof(double) * local_ncols);
 
   // Set the input image
   init_image(nx, ny, width, height, image, tmp_image);
 
-  int lastposition = ((local_nrows * local_ncols)/4)
-  //initialize kernel
-  if(rank == MASTER){
-    for(int i = 0;i<lastposition;i++){
-      section[i] = image[i];
-    }
-  }
-  else if(rank != MASTER && rank <3){
-    for(int j = lastposition;j<(lastposition*(rank +1));j++){
-      section[j] = image[j];
-    }
-  }
-  else if(rank == 3){
-    for(int k = lastposition * 3;k<(local_nrows * local_ncols);k++){
-      section[k] = image[k];
-    }
-  }
-  }
+  for(int ii=0;ii<local_nrows;ii++) {
+   for(int jj=0; jj<local_ncols; jj++) {
+     if (jj > 0 && jj < (local_ncols))
+          section[ii * (local_ncols) + jj] = image[jj + (ii*local_ncols) + (rank*(local_nrows - 2)*(local_ncols))];
+          tmp_section[ii * (local_ncols) + jj] = image[jj + (ii*local_ncols) + (rank*(local_nrows - 2)*(local_ncols))];
+     else if (jj == 0 || jj == (local_ncols + 1))
+            section[ii * (local_ncols + 2) + jj] = 0;
+            tmp_section[ii * (local_ncols + 2) + jj] = 0;
+ }
+}
 
   // Call the stencil kernel
   double tic = wtime();
   for (int t = 0; t < niters; ++t) {
-    stencil(nx, ny, width, height, image, tmp_image);
-    stencil(nx, ny, width, height, tmp_image, image);
+    MPI_Sendrecv(&section[(local_ncols+1)], local_ncols, MPI_FLOAT, up, tag, &section[local_nrows-1 * local_ncols+1], local_ncols, MPI_FLOAT, down, tag, MPI_COMM_WORLD, &status);
+    MPI_Sendrecv(&section[local_nrows-1 * local_ncols+1], local_ncols, MPI_FLOAT, down, tag, &section[1], local_ncols, MPI_FLOAT, up, tag, MPI_COMM_WORLD, &status);
+    stencil(local_nrows-2, local_ncols-2, width, height, section, tmp_section);
+    MPI_Sendrecv(&tmp_section[(local_ncols+1)], local_ncols, MPI_FLOAT, up, tag, &tmp_section[local_nrows-1 * local_ncols+1], local_ncols, MPI_FLOAT, down, tag, MPI_COMM_WORLD, &status);
+    MPI_Sendrecv(&tmp_section[local_nrows-1 * local_ncols+1], local_ncols, MPI_FLOAT, down, tag, &tmp_section[1], local_ncols, MPI_FLOAT, up, tag, MPI_COMM_WORLD, &status);
+    stencil(local_nrows-2, local_ncols-2, width, height, tmp_section, section);
   }
   double toc = wtime();
 
+  if(rank == MASTER){
+    for(int i=0;i<local_nrows;i++){
+      for(int j=0;j<local_ncols;j++){
+        image[j + (i*local_ncols)] = section[i * (local_ncols) + j];
+      }
+    }
+    for(kk = 1;kk<size;kk++){
+      int nrows = calc_nrows_from_rank(kk,size,nx);
+      for(int i =0;i<nrows;i++){
+        MPI_Recv(&image[ ((kk * local_nrows) + i) * width + 1], local_ncols, MPI_FLOAT, kk, tag, MPI_COMM_WORLD, &status);
+      }
+    }
+
+  }
+  else{
+        MPI_Send(&section[i * (local_ncols) + 1], local_ncols, MPI_FLOAT, MASTER, tag, MPI_COMM_WORLD);
+  }
   // Output
+if(rank == MASTER ){
   printf("------------------------------------\n");
   printf(" runtime: %lf s\n", toc - tic);
-  printf("------------------------------------\n");
-
+  printf("------------------------------------\n");}
   output_image(OUTPUT_FILE, nx, ny, width, height, image);
   free(image);
   free(tmp_image);
+  free(section);
+  free(tmp_section);
+  MPI_Finalize();
 }
 
 void stencil(const int nx, const int ny, const int width, const int height,
              float* image, float* tmp_image)
 {
-  for (int i = 1; i < ny + 1; ++i) {
-    for (int j = 1; j < nx + 1; ++j) {
+  for (int i = 1; i < ny - 1; ++i) {
+    for (int j = 1; j < nx - 1; ++j) {
 	int x = j+i*height;
 	tmp_image[x]=(image[x]*0.6f)+((image[x-1]+image[x+1]+image[x+height]+image[x-height])*0.1f);
     }
@@ -190,16 +213,12 @@ double wtime(void)
   gettimeofday(&tv, NULL);
   return tv.tv_sec + tv.tv_usec * 1e-6;
 }
-
-int calc_ncols_from_rank(int rank, int size, int width)
+int calc_ncols_from_rank(int rank, int size, int nx)
 {
-  int ncols;
-
-  ncols = width / size;       /* integer division */
-  if ((width % size) != 0) {  /* if there is a remainder */
+  ncols = nx / size;       /* integer division */
+  if ((nx % size) != 0) {  /* if there is a remainder */
     if (rank == size - 1)
-      ncols += width % size;  /* add remainder to last rank */
+      ncols += nx % size;  /* add remainder to last rank */
   }
-
   return ncols;
 }
